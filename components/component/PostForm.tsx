1b5eed50469475cb0d98b6e43cef64e390612368
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-07 10:28:42 +0900

    ✨バリデーションエラーを出力

--- components/component/PostForm.tsx
+++ components/component/PostForm.tsx
@@ -1,4 +1,5 @@
-// components/PostForm.tsx
+"use client";
+
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -6,69 +7,51 @@ import { SendIcon } from "./Icons";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { z } from "zod";
+import { useRef, useState } from "react";
+import { addPostAction } from "@/lib/actions";
 
 export default function PostForm() {
-  async function addPostAction(formData: FormData) {
-    "use server";
+  const [error, setError] = useState<string | undefined>("");
+  const formRef = useRef<HTMLFormElement>(null);
 
-    try {
-      const { userId } = auth();
-
-      if (!userId) {
-        return;
-      }
-      const postText = formData.get("post") as string;
-      const postTextSchema = z
-        .string()
-        .min(1, "ポスト内容を入力してください。")
-        .max(140, "140字以内で入力してください");
-
-      const validatedPostText = postTextSchema.parse(postText);
-
-      await prisma.post.create({
-        data: {
-          content: validatedPostText,
-          authorId: userId,
-        },
-      });
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return {
-          error: error.errors.map((e) => e.message).join(", "),
-          success: false,
-        };
-      } else if (error instanceof Error) {
-        return {
-          error: error.message,
-          success: false,
-        };
-      } else {
-        return {
-          error: "予期せぬエラーが発生しました。",
-          success: false,
-        }
+  const handleSubmit = async (formData: FormData) => {
+    const result = await addPostAction(formData);
+    if (!result?.success) {
+      setError(result?.error);
+    } else {
+      setError("");
+      if (formRef.current) {
+        formRef.current.reset();
       }
     }
-  }
+  };
 
   return (
-    <div className="flex items-center gap-4">
-      <Avatar className="w-10 h-10">
-        <AvatarImage src="/placeholder-user.jpg" />
-        <AvatarFallback>AC</AvatarFallback>
-      </Avatar>
-      <form action={addPostAction} className="flex items-center flex-1">
-        <Input
-          type="text"
-          placeholder="What's on your mind?"
-          className="flex-1 rounded-full bg-muted px-4 py-2"
-          name="post"
-        />
-        <Button variant="ghost" size="icon">
-          <SendIcon className="h-5 w-5 text-muted-foreground" />
-          <span className="sr-only">Tweet</span>
-        </Button>
-      </form>
+    <div>
+      <div className="flex items-center gap-4">
+        <Avatar className="w-10 h-10">
+          <AvatarImage src="/placeholder-user.jpg" />
+          <AvatarFallback>AC</AvatarFallback>
+        </Avatar>
+        <form
+          ref={formRef}
+          action={handleSubmit}
+          className="flex items-center flex-1"
+        >
+          <Input
+            type="text"
+            placeholder="What's on your mind?"
+            className="flex-1 rounded-full bg-muted px-4 py-2"
+            name="post"
+          />
+          <Button variant="ghost" size="icon">
+            <SendIcon className="h-5 w-5 text-muted-foreground" />
+            <span className="sr-only">Tweet</span>
+          </Button>
+        </form>
+      </div>
+
+      {error && <p className="text-destructive mt-1 ml-14">{error}</p>}
     </div>
   );
 }
---
+++ lib/actions.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
+import prisma from "./prisma";
+import { error } from "console";
+
+export async function addPostAction(formData: FormData) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return;
+    }
+    const postText = formData.get("post") as string;
+    const postTextSchema = z
+      .string()
+      .min(1, "ポスト内容を入力してください。")
+      .max(140, "140字以内で入力してください");
+
+    const validatedPostText = postTextSchema.parse(postText);
+
+    await prisma.post.create({
+      data: {
+        content: validatedPostText,
+        authorId: userId,
+      },
+    });
+    return {
+      error: undefined,
+      success: true,
+    };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return {
+        error: error.errors.map((e) => e.message).join(", "),
+        success: false,
+      };
+    } else if (error instanceof Error) {
+      return {
+        error: error.message,
+        success: false,
+      };
+    } else {
+      return {
+        error: "予期せぬエラーが発生しました。",
+        success: false,
+      };
+    }
+  }
+}
