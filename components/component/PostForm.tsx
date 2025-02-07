a22df60819d34e992b2b2956079b119a3109e045
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-07 09:48:59 +0900

    ✨ServerActionsとPrismaを使ってSupabaseにデータを登録

--- components/component/PostForm.tsx
+++ components/component/PostForm.tsx
@@ -3,24 +3,50 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { SendIcon } from "./Icons";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 
 export default function PostForm() {
+  const { userId } = auth();
+
+  async function addPostAction(formData: FormData) {
+    "use server";
+    const postText = formData.get("post") as string;
+
+    if (!userId) {
+      return;
+    }
+
+    try {
+      await prisma.post.create({
+        data: {
+          content: postText,
+          authorId: userId,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="flex items-center gap-4">
       <Avatar className="w-10 h-10">
         <AvatarImage src="/placeholder-user.jpg" />
         <AvatarFallback>AC</AvatarFallback>
       </Avatar>
-      <Input
-        type="text"
-        placeholder="What's on your mind?"
-        className="flex-1 rounded-full bg-muted px-4 py-2"
-        name="post"
-      />
-      <Button variant="ghost" size="icon">
-        <SendIcon className="h-5 w-5 text-muted-foreground" />
-        <span className="sr-only">Tweet</span>
-      </Button>
+      <form action={addPostAction} className="flex items-center flex-1">
+        <Input
+          type="text"
+          placeholder="What's on your mind?"
+          className="flex-1 rounded-full bg-muted px-4 py-2"
+          name="post"
+        />
+        <Button variant="ghost" size="icon">
+          <SendIcon className="h-5 w-5 text-muted-foreground" />
+          <span className="sr-only">Tweet</span>
+        </Button>
+      </form>
     </div>
   );
 }
