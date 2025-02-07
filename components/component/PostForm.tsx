7ece8ed888f5cf03a29a3333e3c60495162e4bb3
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-07 10:09:09 +0900

    ✨zodを使ってバリデーションエラー

--- components/component/PostForm.tsx
+++ components/component/PostForm.tsx
@@ -5,27 +5,49 @@ import { Button } from "@/components/ui/button";
 import { SendIcon } from "./Icons";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
 
 export default function PostForm() {
-  const { userId } = auth();
-
   async function addPostAction(formData: FormData) {
     "use server";
-    const postText = formData.get("post") as string;
-
-    if (!userId) {
-      return;
-    }
 
     try {
+      const { userId } = auth();
+
+      if (!userId) {
+        return;
+      }
+      const postText = formData.get("post") as string;
+      const postTextSchema = z
+        .string()
+        .min(1, "ポスト内容を入力してください。")
+        .max(140, "140字以内で入力してください");
+
+      const validatedPostText = postTextSchema.parse(postText);
+
       await prisma.post.create({
         data: {
-          content: postText,
+          content: validatedPostText,
           authorId: userId,
         },
       });
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return {
+          error: error.errors.map((e) => e.message).join(", "),
+          success: false,
+        };
+      } else if (error instanceof Error) {
+        return {
+          error: error.message,
+          success: false,
+        };
+      } else {
+        return {
+          error: "予期せぬエラーが発生しました。",
+          success: false,
+        }
+      }
     }
   }
 
